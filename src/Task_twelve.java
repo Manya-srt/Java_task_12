4eb967dbbcf5b75114d82410e2d6a58abeb0e4ab
Mariya27 <mariya.akulova.91@inbox.ru>
2019-01-27 23:43:56 +0600

    Initial commit

---
+++ src/Task_twelve.java
@@ -0,0 +1,30 @@
+// Необходимо провести сравнение длинны двух строк,
+// которые были введены с клавиатуры и записаны в соответствующие переменные.
+// Вывести на экран строку с наибольшей длинной.
+
+import java.util.Scanner;
+
+public class Task_twelve {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое сообщение");
+        String sms = scanner.nextLine();
+        int len = sms.length();
+
+        System.out.println("Введите второе сообщение");
+        String sms2 = scanner.nextLine();
+        int len2 = sms2.length();
+
+// Сравнение длин строк s1 и s2.
+        if (len > len2) {
+            System.out.println("Результат " + sms);
+        }
+        if (len < len2) {
+            System.out.println("Результат " + sms2);
+        }
+        else {
+            System.out.println("Строки " + sms + " и " + sms2 + " равны.");
+        }
+    }
+}
